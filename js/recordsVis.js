89c37713063085cf37d6a8e77319daa9ae0ca993
rohanssheth <rohanssheth@hotmail.com>
2022-11-21 03:01:06 -0500

    committed

--- js/recordsVis.js
+++ js/recordsVis.js
@@ -1,9 +1,8 @@
 class RecordsVis {
 
-    constructor(parentElement, data, geoData) {
+    constructor(parentElement, data) {
         this.parentElement = parentElement;
         this.data = data;
-        this.geoData = geoData
 
         this.initVis()
     }
@@ -21,70 +20,6 @@ class RecordsVis {
             .attr("height", vis.height)
             .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
 
-        // create projection
-        vis.projection = d3.geoOrthographic()
-            // vis.projection = d3.geoStereographic() was interesting - but probably less intuitive than geoOrthographic
-            // guess-and-check to scale size
-            .scale(vis.height * .42)
-            .translate([vis.width / 2, vis.height / 2])
-
-        // pass projection to geogenerator
-        vis.path = d3.geoPath()
-            .projection(vis.projection);
-
-        // code for building sphere
-        vis.svg.append("path")
-            .datum({type: "Sphere"})
-            .attr("class", "graticule")
-            .attr('fill', '#ADDEFF')
-            .attr("stroke","rgba(129,129,129,0.35)")
-            .attr("d", vis.path);
-
-        // convert topoJSON to GeoJSON
-        vis.world = topojson.feature(vis.geoData, vis.geoData.objects.countries).features
-
-        // draw countries
-        vis.countries = vis.svg.selectAll(".country")
-            .data(vis.world)
-            .enter().append("path")
-            .attr('class', 'country')
-            .attr("d", vis.path)
-
-        // append tooltip (Same as pie chart)
-        vis.tooltip = d3.select("body").append('div')
-            .attr('class', "tooltip")
-            .attr('id', 'pieTooltip')
-
-        // creating legend
-        vis.legend = vis.svg.append("g")
-            .attr('class', 'legend')
-            .attr('transform', `translate(${vis.width * 2.7 / 4}, ${vis.height - 40})`)
-
-        // step 10, free code: making map draggable
-        let m0,
-            o0;
-
-        vis.svg.call(
-            d3.drag()
-                .on("start", function (event) {
-
-                    let lastRotationParams = vis.projection.rotate();
-                    m0 = [event.x, event.y];
-                    o0 = [-lastRotationParams[0], -lastRotationParams[1]];
-                })
-                .on("drag", function (event) {
-                    if (m0) {
-                        let m1 = [event.x, event.y],
-                            o1 = [o0[0] + (m0[0] - m1[0]) / 4, o0[1] + (m1[1] - m0[1]) / 4];
-                        vis.projection.rotate([-o1[0], -o1[1]]);
-                    }
-
-                    // Update the map
-                    vis.path = d3.geoPath().projection(vis.projection);
-                    d3.selectAll(".country").attr("d", vis.path)
-                    d3.selectAll(".graticule").attr("d", vis.path)
-                })
-        )
 
         vis.wrangleData()
 
@@ -93,13 +28,6 @@ class RecordsVis {
     wrangleData() {
         let vis = this;
 
-        // create random data structure with information for each land
-        vis.countryInfo = {};
-        vis.geoData.objects.countries.geometries.forEach(d => {
-            vis.countryInfo[d.properties.name] = {
-                name: d.properties.name,
-            }
-        })
 
         vis.updateVis()
     }
@@ -107,34 +35,6 @@ class RecordsVis {
     updateVis() {
         let vis = this;
 
-        // based off example from pie chart earlier, using countryInfo data above
-        vis.countries.on('mouseover', function(event, d){
-            d3.select(this)
-                .attr('stroke-width', '2px')
-                .attr('stroke', 'black')
-                .style('fill', 'rgba(173,222,255,0.62)')
 
-            vis.tooltip
-                .style("opacity", 1)
-                .style("left", event.pageX + 20 + "px")
-                .style("top", event.pageY + "px")
-                // going off example from screenshot
-                .html(`
-                        <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 20px">
-                        <h3>${d.properties.name}<h3>                       
-                        </div>`);
-        })
-            // mouseout code following lab page instructions
-            .on('mouseout', function(event, d){
-                d3.select(this)
-                    .attr('stroke-width', '0px')
-                    .style("fill", vis.countryInfo[d.properties.name].color)
-
-                vis.tooltip
-                    .style("opacity", 0)
-                    .style("left", 0)
-                    .style("top", 0)
-                    .html(``);
-            })
     }
 }
